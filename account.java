7e20bb74fdf85298ea5bdce39baf5ac6afac9bd2
Startup-Ideas5 <startupideas56@gmail.com>
2025-08-09 23:36:22 +0530

    new project

---
+++ account.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public interface account {
+   bankAccount createAccount(HashMap<Integer,bankAccount> acc);
+
+    void deposit();
+
+    void withdraw();
+
+    long checkBalance();
+
+    void displayAccountDetails();
+}
